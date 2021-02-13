da3c34f113c656b99098c4dfaabda13e709ca94d
Robotechnic <robotechnic.developeur@gmail.com>
2021-02-13 10:29:46 +0100

    moove exemple to example folder and remove vs config files

---
+++ example/bounceMouse/src/ofApp.cpp
@@ -0,0 +1,89 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetFrameRate(60);
+    ofSetWindowShape(ofGetScreenWidth() / 3, ofGetScreenHeight() / 3);
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    
+    x = ofGetWidth() * 0.5;
+    y = 10;
+    vx = 0;
+    vy = 0;
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+    vy += 0.3;
+    vx *= 0.9;
+    x += vx;
+    y += vy;
+    if (x < 5) {
+        x = 10;
+        vx *= -1;
+        vx *= 0.7;
+    }
+    if (x > ofGetWidth() - 5) {
+        x = ofGetWidth() - 5;
+        vx *= -1;
+        vx *= 0.7;
+    }
+    if (y < 5) {
+        y = 10;
+        vy *= -1;
+    }
+    if (y > ofGetHeight() - 5) {
+        y = ofGetHeight() - 5;
+        vy *= -1;
+        vy *= 0.7;
+    }
+    
+    // set mouse position
+    ofxMouseController::setPos(x * 3, y * 3);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    ofBackground(255);
+    
+    ofFill();
+    ofSetColor(0);
+    ofCircle(x, y, 8);
+    
+    stringstream s;
+    s << "ofxMouseController DEMO" << endl;
+    s << "this is the your mouse." << endl;
+    s << "perform action" << endl;
+    s << "- KEY UP    : jump" << endl;
+    s << "- KEY LEFT  : move left" << endl;
+    s << "- KEY RIGHT : move right" << endl;
+    s << "- KEY 1     : left click" << endl;
+    s << "- KEY 2     : right click" << endl;
+    s << "- KEY ESC   : exit";
+    ofDrawBitmapString(s.str(), 10, 20);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch (key) {
+        case OF_KEY_UP:     vy = -5;    break;
+        case OF_KEY_LEFT:   vx -= 5;    break;
+        case OF_KEY_RIGHT:  vx += 5;    break;
+        case '1': ofxMouseController::click();        break;
+        case '2': ofxMouseController::clickRight();   break;
+        case '3': ofxMouseController::buttonDown();   break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    switch (key) {
+        case '3': ofxMouseController::buttonUp();     break;
+    }
+}
+
---
+++ example/bounceMouse/src/ofApp.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxMouseController.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+    
+    float x, y, vx, vy;    
+		
+};
